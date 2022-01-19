c81631bf1247fdba42d9e2a7a5dafbc512f1f495
StefanyCandida <stefany.candida@gmail.com>
2022-01-19 15:50:41 -0300

    atividade 2

---
+++ script.js
@@ -0,0 +1,40 @@
+
+let idade = 33
+
+if (idade >= 18) {
+    console.log("Parabéns, maior de idade")
+} else {
+    console.log("Você ainda é menor de idade")
+}
+let humano = true 
+if (idade >= 18 && humano === true){
+   console.log("Você é humano maior de 18 anos ")
+} else {
+    console.log("Você é um humano menor de idade")
+    }
+let mes = "fevereiro"
+if ( mes === "dezembro" || mes ==="janeiro"){
+console.log("Você nasceu em dezembro ou janeiro")
+}else{
+ console.log("Você não nasceu em dezembro ou janeiro")}
+
+
+
+
+let nome = "Stéfany"
+let Sobrenome = "Candida"
+
+
+if (nome.substring(0,1)=== "R"){
+    console.log("Seu Nome começa com a letra R")
+}else{
+    console.log("Seu nome não começa com a letra R")
+
+
+if (Sobrenome.length > 6 || nome.substring(0,1)=== "E"){
+    console.log("Seu sobrenome tem mais de 6 letras e começa com a letra E")
+}else{
+    console.log("Seu nome não tem mais de 6 letras ou não começa com a letra E")
+}
+
+}
